d010dca9c3633fd5cba296e396e8bec88e9e3e7b
Hein-Carl Frijlink <hc.frijlink@freedom.nl>
2023-12-06 11:44:04 +0100

    first test

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# poc-restassured-net
-Proof of Concept RestAssured.Net
+# Proof of Concept RestAssured.Net
+
+Let's test [RestAssured.Net](https://github.com/basdijkstra/rest-assured-net)
+
+## Environment Variables ##
+
+| key              | value                   |
+|------------------|-------------------------|
+| RETRIES          | "0"                     |
+| TRELLO_API_URL   |"https://api.trello.com" |
+| TRELLO_API_KEY   |                         |
+| TRELLO_API_TOKEN |                         |
---
+++ Tests/MembersTests.cs
@@ -0,0 +1,24 @@
+using static Settings.Configuration;
+
+namespace Tests;
+
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public class MembersTests
+{
+    [Test, Category("API")]
+    public void RetrieveAmountOfBoardsFromMember()
+    {
+        var baseUrl = GetEnvironmentVariable("TRELLO_API_URL");
+        var key = GetEnvironmentVariable("TRELLO_API_KEY");
+        var token = GetEnvironmentVariable("TRELLO_API_TOKEN");
+
+        Given()
+            .PathParam("key", key)
+            .PathParam("token", token)
+            .When()
+            .Get($"{baseUrl}/1/members/me/boards?key=[key]&token=[token]")
+            .Then()
+            .StatusCode(200);
+    }
+}
